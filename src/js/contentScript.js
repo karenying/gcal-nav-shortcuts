b53897b11303db3f263a9077cfebc2987c319963
Karen Ying <karenying7@gmail.com>
2020-07-05 11:35:06 -0400

    Add error handling and fix formatting

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -1,10 +1,13 @@
 const container = document.getElementsByClassName('rbGOge SeRypc');
-const containerChildren = container[0].childNodes;
 
-const leftArrow = containerChildren[1];
-const rightArrow = containerChildren[2];
+if (container) {
+    const containerChildren = container[0].childNodes;
 
-chrome.runtime.onMessage.addListener(function (request) {
-    const { direction } = request;
-    direction === 'left' ? leftArrow.click() : rightArrow.click();
-});
+    const leftArrow = containerChildren[1];
+    const rightArrow = containerChildren[2];
+
+    chrome.runtime.onMessage.addListener(function (request) {
+        const { direction } = request;
+        direction === 'left' ? leftArrow.click() : rightArrow.click();
+    });
+}
