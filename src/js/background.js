1b6f21581ae63aad7117f645daf75a57841a4002
Karen Ying <karenying7@gmail.com>
2020-07-05 12:00:13 -0400

    Refactor

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,5 @@
 chrome.commands.onCommand.addListener(function (command) {
-    const direction = command.slice(9);
+    const direction = command.split('-')[1];
 
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { direction });
